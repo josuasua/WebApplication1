f07f00e7738f86c5af355a2b6a98c7de44944510
josuasua <josuasua@hotmail.com>
2016-10-04 14:53:24 +0200

    lalala

--- WebApplication1/index.aspx.designer.cs
+++ WebApplication1/index.aspx.designer.cs
@@ -22,6 +22,15 @@ namespace WebApplication1 {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// Control Menu1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Menu Menu1;
+        
+        /// <summary>
         /// Control grdv_Usuarios.
         /// </summary>
         /// <remarks>
