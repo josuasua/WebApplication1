f780e4f5995bec9b6065beeafa137d85e6e54be9
josuasua <josuasua@hotmail.com>
2016-10-04 14:17:22 +0200

    Agregar archivos de proyecto.

---
+++ WebApplication1/index.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="WebApplication1.index" %>
+
+<!DOCTYPE html>
+
+<html lang="es-es">
+<head runat="server">
+<meta charset="utf-8" />
+    <title></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:GridView OnRowCommand="grdv_Usuarios_RowCommand" ID="grdv_Usuarios" runat="server" AllowPaging="True" AllowSorting="True" AutoGenerateColumns="False" DataKeyNames="idUsuario" DataSourceID="SqlDataSource1">
+            <Columns>
+                <asp:ButtonField CommandName="editUsuario"  Text="Editar" ControlStyle-CssClass="btn btn-info" />
+                    
+                <asp:ButtonField CommandName="borrarUsuario"  Text="Borrar" ControlStyle-CssClass="btn btn-danger" />
+       
+                <asp:BoundField  DataField="idUsuario" />
+            </Columns>
+        </asp:GridView>
+ 
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/index.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WebApplication1
+{
+    public partial class index : System.Web.UI.Page
+    {
+        DataTable dt;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            cargaDatos();
+
+        }
+
+        private void cargaDatos()
+        {
+            try
+            {
+                string cadena = ConfigurationManager.ConnectionStrings["GESTLIBRERIAConnectionString"].ConnectionString;
+                string SQL = "SELECT * FORM usuario";
+                SqlConnection conn = new SqlConnection(cadena);
+                conn.Open();
+                DataSet ds = new DataSet();
+                SqlDataAdapter dAdapter = new SqlDataAdapter(SQL, conn);
+
+                dAdapter.Fill(ds);
+                dt = ds.Tables[0];
+
+                grdv_Usuarios.DataSource = dt;
+                grdv_Usuarios.DataBind();
+                conn.Close();
+            }
+            catch (SqlException ex)
+            {
+                System.Console.Error.Write(ex.Message);
+            }
+            
+        }
+
+        protected void grdv_Usuarios_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            //int codigo;
+            string command = e.CommandName;
+            switch (command)
+            {
+                case "editUsuario":
+                    //hemos pulsado el boton de editar
+                    break;
+                case "borrarUsuario":
+                    //hemos pulsado el boton de borrar
+                    break;
+
+            }
+                
+        }
+    }
+}
